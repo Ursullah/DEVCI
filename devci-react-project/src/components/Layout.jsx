2215d31e5abeb7d6e8b7ab99f459390a9892463f
Ursullah <khanguhaursula@gmail.com>
2025-03-20 22:57:05 +0300

    Changed background color to -gary-400

--- devci-react-project/src/components/Layout.jsx
+++ devci-react-project/src/components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = () => {
     <div>
       {/* 🔹 Persistent Header */}
       <header className="bg-slate-800 text-white py-4 px-6 flex justify-between items-center">
-        <h1 className="font-bold text-3xl text-black">DEVCI</h1>
+        <h1 className="font-bold text-3xl text-red-600">DEVCI</h1>
         <nav>
           <ul className="flex space-x-4">
             <li><Link to="/" className="hover:underline text-2xl text-white font-bold">Home</Link></li>
